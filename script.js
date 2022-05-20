559346e7792de4eb8faecc76d57457dbe4216820
Rafaela Carvalho <87669847+rafaelahc@users.noreply.github.com>
2022-05-20 11:19:31 +0100

    Update script.js
    Deleted age function

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-//Idade
-function calculaIdade(dataNasc){ 
-    var dataAtual = new Date();
-    var anoAtual = dataAtual.getFullYear();
-    var anoNascParts = dataNasc.split('/');
-    var diaNasc =anoNascParts[0];
-    var mesNasc =anoNascParts[1];
-    var anoNasc =anoNascParts[2];
-    var idade = anoAtual - anoNasc;
-    var mesAtual = dataAtual.getMonth() + 1; 
-    //Se mes atual for menor que o nascimento, nao fez aniversario ainda;  
-    if(mesAtual < mesNasc){
-    idade--; 
-    } else {
-    //Se estiver no mes do nascimento, verificar o dia
-    if(mesAtual == mesNasc){ 
-    if(new Date().getDate() < diaNasc ){ 
-    //Se a data atual for menor que o dia de nascimento ele ainda nao fez aniversario
-    idade--; 
-    }
-    }
-    } 
-    return idade;
-}
-console.log(calculaIdade('29/05/1995'));
-
-
 
 //MENU ACTIVE
 $('.nav-item').on('click', function() {
